3b62a863c0005f47c04dc0fac4f82c190b3d147e
unknown <chamberbankes@gmail.com>
2024-08-30 09:56:58 -0400

    about me rough

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { About, Contact, Portfolio,Resume } from "./components/pages";
 
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export const About = () => {
-  return <h1>About Me</h1>;
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    
+  };
+
+  const textStyle = {
+    flex: 1,
+    marginRight: "20px",
+  };
+
+  const imageStyle = {
+    flex: 1,
+  };
+
+  return (
+    <div style={containerStyle}>
+   
+      <div style={imageStyle}>
+        <img src="../assets/images/gnomecode.png" alt="Gnomecoding" style={{ width: "100%", borderRadius: "8px" }} />
+      </div>
+      <div style={textStyle}>
+        <h1>About Me</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna at nisi volutpat fermentum. Phasellus ut condimentum orci. Aenean ac magna in felis dapibus tempus.</p>
+      </div>
+    </div>
+  );
 };
